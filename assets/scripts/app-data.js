6e31e8f231c44ea50c82a0dab4649d3e6ed52840
OllieBuilds <scarborough.oliver@gmail.com>
2016-06-07 09:16:31 -0400

    Creates basic layout

---
+++ assets/scripts/app-data.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const app = {
+  // api: 'https://surfsapp.herokuapp.com/',
+  api: 'http://localhost:3000',
+  // msw: 'http://magicseaweed.com/api/c1b08a0ce3e6bfa59407d633d5ca0a10/forecast/?spot_id=369'
+
+
+};
+
+module.exports = app;
