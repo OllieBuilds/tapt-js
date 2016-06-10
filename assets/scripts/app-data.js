5ba3262e2ad1f210a51ac4b5c4532fede9b1b746
OllieBuilds <scarborough.oliver@gmail.com>
2016-06-10 13:02:46 -0400

    Changes api url from local to heroku

--- assets/scripts/app-data.js
+++ assets/scripts/app-data.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const app = {
-  api: 'http://localhost:3000/',
+  api: 'https://rocky-castle-89877.herokuapp.com/',
+  // api: 'http://localhost:3000/',
   // brewKey: '?key=1e4230aee7ebf8eb42550ddfdcae3e1e',
   // brew: 'http://api.brewerydb.com/v2/',
 };
